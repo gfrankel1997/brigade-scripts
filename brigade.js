a191377d78b76cae95ef05345443776614940368
gfrankel97 <gabriel.frankel@getbeyond.com>
2019-04-12 15:53:11 -0400

    brigade should do something now

--- brigade.js
+++ brigade.js
@@ -1,26 +1,27 @@
 const { events, Job } = require("brigadier");
 console.log('HEREEEEEEE')
 events.on("simpleevent", (event, project) => {
+    console.log('I AM INSIDE')
     console.log(event)
     console.log(project)
-    // var job = new Job("blah-test", "regbatchapps.azurecr.io/batchapps/kubernetes");
+    var job = new Job("blah-test", "regbatchapps.azurecr.io/batchapps/kubernetes");
 
-    // job.tasks = [
-    //     "cat /scripts/start.sh"
-    // ];
+    job.tasks = [
+        "cat /scripts/start.sh"
+    ];
 
-    // job.env = {
-    //     "DOWNLOAD_FILE": "False",
-    //     "APP_PATH": "/apps/ADF",
-    //     "BATCH_COMMAND": "Beyond.Compensation.AutoReview.Console.dll",
-    //     "ASPNETCORE_ENVIRONMENT": "development",
-    //     "APP_COMMAND": "dotnet",
-    //     "LOG_FILE_NAME": "blah2.txt"
-    // };
+    job.env = {
+        "DOWNLOAD_FILE": "False",
+        "APP_PATH": "/apps/ADF",
+        "BATCH_COMMAND": "Beyond.Compensation.AutoReview.Console.dll",
+        "ASPNETCORE_ENVIRONMENT": "development",
+        "APP_COMMAND": "dotnet",
+        "LOG_FILE_NAME": "blah2.txt"
+    };
 
-    // job.run().then((res) => {
-    //     console.log(res);
-    // }).catch((err) => {
-    //     console.log(err);
-    // });
+    job.run().then((res) => {
+        console.log(res);
+    }).catch((err) => {
+        console.log(err);
+    });
   });

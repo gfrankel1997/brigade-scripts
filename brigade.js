6ff8841723d4c8117ef15e8a3cf1826d39e42f27
gfrankel97 <gabriel.frankel@getbeyond.com>
2019-04-15 15:55:04 -0400

    added node modules

--- brigade.js
+++ brigade.js
@@ -1,10 +1,20 @@
 const { events, Job } = require("brigadier");
-events.on("simpleevent", (event, project) => {
+const appInsights = require('applicationinsights');
+const settings = require('./appsettings.json');
 
+appInsights.setup(settings.APPINSIGHTS_INSTRUMENTATIONKEY).start();
+var client = appInsights.defaultClient;
+console.log('HERE')
+client.trackTrace({message: "Brigade invoked"});
+
+events.on("exec", (event, project) => {
+    client.trackTrace({message: "Brigade event " + event + "received with payload: " + event.payload});
     console.log("EVENT: ", event);
     console.log('PAYLOAD: ', event.payload)
     var mypayload = JSON.parse(event.payload);
-    console.log('MY PAYLOAD: ', mypayload)
+    console.log('MY PAYLOAD: ', mypayload);
+
+    console.log('SETTINGS: ', settings);
 
     var job = new Job("gateway-test", "regbatchapps.azurecr.io/batchapps/generic6");
     job.imagePullSecrets = ["myregistrykey"];
@@ -17,7 +27,9 @@ events.on("simpleevent", (event, project) => {
 
     job.run().then((res) => {
         console.log('SUCCESS: ', res);
+        client.trackTrace({message: "Brigade event " + event + "succeeded"});
     }).catch((err) => {
+        client.traceException({exception: new Error("Brigade event " + event + "failed with error(s) " + err.toString())})
         console.log('ERROR: ', err);
     });
-  });
+});

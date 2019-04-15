e4d84560f3768716fb529ffb874021aa9e9b6743
gfrankel97 <gabriel.frankel@getbeyond.com>
2019-04-15 10:22:27 -0400

    force name change

--- brigade.js
+++ brigade.js
@@ -6,7 +6,7 @@ events.on("simpleevent", (event, project) => {
     var mypayload = JSON.parse(event.payload);
     console.log('MY PAYLOAD: ', mypayload)
 
-    var job = new Job("gateway-test", "regbatchapps.azurecr.io/batchapps/generic:latest");
+    var job = new Job("gateway-test", "regbatchapps.azurecr.io/batchapps/generic2");
     job.imagePullSecrets = ["myregistrykey"];
 
     job.tasks = [

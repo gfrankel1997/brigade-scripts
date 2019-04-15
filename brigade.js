6cd6c0c7bd602ed8138ebacd448b5e41f4390667
gfrankel97 <gabriel.frankel@getbeyond.com>
2019-04-15 09:17:19 -0400

    added more stuff to brigade

--- brigade.js
+++ brigade.js
@@ -1,23 +1,27 @@
 const { events, Job } = require("brigadier");
-console.log('HEREEEEEEE')
 events.on("simpleevent", (event, project) => {
-    console.log('I AM INSIDE')
-    console.log(event)
-    console.log(project)
-    var job = new Job("blah-test", "regbatchapps.azurecr.io/batchapps/kubernetes");
+
+    console.log("EVENT: ", event);
+    console.log('PAYLOAD: ', event.payload)
+
+    var job = new Job("gateway-test", "regbatchapps.azurecr.io/batchapps/generic");
 
     job.tasks = [
         "cat /scripts/start.sh"
     ];
 
     job.env = {
-        "DOWNLOAD_FILE": "False",
-        "APP_PATH": "/apps/ADF",
-        "BATCH_COMMAND": "Beyond.Compensation.AutoReview.Console.dll",
-        "ASPNETCORE_ENVIRONMENT": "development",
-        "APP_COMMAND": "dotnet",
-        "LOG_FILE_NAME": "blah2.txt"
+        "APP_URL": event.payload.APP_URL
     };
+
+    // job.env = {
+    //     "DOWNLOAD_FILE": "False",
+    //     "APP_PATH": "/apps/PostCAM",
+    //     "BATCH_COMMAND": "Beyond.Compensation.AutoReview.Console.dll",
+    //     "ASPNETCORE_ENVIRONMENT": "development",
+    //     "APP_COMMAND": "dotnet",
+    //     "LOG_FILE_NAME": "blah2.txt"
+    // };
 
     job.run().then((res) => {
         console.log(res);

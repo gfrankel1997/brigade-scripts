db30eb0b68d7c3adf7566d210c89a0772d8dac41
gfrankel97 <gabriel.frankel@getbeyond.com>
2019-04-17 10:15:33 -0400

    oops

--- brigade.js
+++ brigade.js
@@ -14,7 +14,7 @@ events.on("simpleevent", (event, project) => {
     var mypayload = JSON.parse(event.payload);
     console.log('MY PAYLOAD: ', mypayload);
     
-    console.log('SETTINGS: ', settings);
+    // console.log('SETTINGS: ', settings);
     
     var job = new Job("gateway-test", "regbatchapps.azurecr.io/batchapps/generic");
     job.imageForcePull = true;

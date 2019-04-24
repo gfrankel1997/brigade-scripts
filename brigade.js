0c7ebc0eafb6b9050b26bbf70f7987b03f9584cf
gfrankel97 <gabriel.frankel@getbeyond.com>
2019-04-24 15:45:06 -0400

    added postcam stuff

--- brigade.js
+++ brigade.js
@@ -26,7 +26,10 @@ events.on("batchfilereceived", (event, project) => {
         if(brigade_payload.env_vars.FILE_NAME.includes("DDF") && brigade_payload.env_vars.FILE_NAME.includes("MIDNGHT")) {
             client.trackTrace({message: "DDF MIDNGHT file processed, starting PostCAM"});
             var postcam_timestamp = Math.floor((new Date()).getTime() / 1000);
-            var postcam_job = create_job("postcam-" + postcam_timestamp, settings.registry + "/" + "batchapps/postcam");
+            var postcam_env_vars = {
+                "SETTINGS_URL": brigade_payload.env_vars.POSTCAM_SETTINGS_URL
+            }
+            var postcam_job = create_job("postcam-" + postcam_timestamp, settings.registry + "/batchapps/postcam", postcam_env_vars);
 
             postcam_job.run().then((res) => {
                 client.trackTrace({message: "Brigade event PostCAM-" + postcam_timestamp + " succeeded"});
